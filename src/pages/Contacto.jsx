dff0e8c2fd367fea59e2cb09d35bcdbc711e8c8c
Mantrox <aguileramati50@gmail.com>
2025-03-26 20:05:13 -0300

    Cambios en estilos del contacto y agrego Toastify

--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -1,6 +1,7 @@
 import { useRef } from "react";
 import { useForm } from "react-hook-form";
-import process from "process";
+import Toastify from "toastify-js";
+import "toastify-js/src/toastify.css";
 import emailjs from "@emailjs/browser";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
@@ -22,16 +23,29 @@ const Contacto = () => {
   });
 
   const onSubmit = (data) => {
-    resetField("email");
-    resetField("formBody");
-
     emailjs
       .send(import.meta.env.VITE_YOUR_SERVICE_ID, import.meta.env.VITE_YOUR_TEMPLATE_ID, data, {
         publicKey: import.meta.env.VITE_YOUR_PUBLIC_KEY
       })
       .then(
         () => {
-          console.log("SUCCESS!");
+          resetField("email");
+          resetField("formBody");
+
+          Toastify({
+            text: "Gracias por contactarte con nosotros!",
+            duration: 3000,
+            destination: "https://github.com/apvarun/toastify-js",
+            newWindow: true,
+            close: true,
+            gravity: "top", // `top` or `bottom`
+            position: "right", // `left`, `center` or `right`
+            stopOnFocus: true, // Prevents dismissing of toast on hover
+            style: {
+              background: "linear-gradient(to right, #00b09b, #96c93d)"
+            },
+            onClick: function () {} // Callback after click
+          }).showToast();
         },
         (error) => {
           console.log("FAILED...", error);
@@ -50,11 +64,11 @@ const Contacto = () => {
             <h3 className="text-4xl">Habla con nosotros</h3>
             <form
               ref={form}
-              className="w-full h-full flex flex-col gap-4 justify-between lg:w-2/3"
+              className="w-full h-full flex flex-col gap-4 justify-between md:w-[80%] lg:w-1/2 "
               onSubmit={handleSubmit(onSubmit)}
             >
               <input
-                className="bg-gray-50 w-full text-sm focus:outline-none md:text-lg lg:text-xl rounded-md pl-2 md:pl-5"
+                className="bg-gray-50 w-full text-sm focus:outline-none md:text-lg  rounded-md pl-2 md:pl-3 md:h-10 lg:text-xl lg:h-15"
                 type="text"
                 placeholder="Tu correo Electrónico"
                 {...register("email", {
@@ -63,16 +77,26 @@ const Contacto = () => {
                 })}
                 aria-invalid={errors.email ? "true" : "false"}
               />
-              {errors.email?.type === "required" && <p role="alert">El email es requerido</p>}
-              {errors?.email?.types?.pattern && <p>Ingresa un email válido.</p>}
+              {errors.email?.type === "required" && (
+                <p className="bg-red-400 text-white pl-3 rounded-md w-1/3" role="alert">
+                  El email es requerido!
+                </p>
+              )}
+              {errors?.email?.types?.pattern && (
+                <p className="bg-red-400 text-white pl-3 rounded-md w-1/3">Ingresa un email válido.</p>
+              )}
               <textarea
-                className="bg-gray-50 w-full text-sm focus:outline-none md:text-lg lg:text-xl rounded-md max-h-[10rem] min-h-[1rem] pl-2 pt-1 md:pl-5"
+                className="bg-gray-50 w-full text-sm focus:outline-none md:text-lg rounded-md max-h-[10rem] min-h-[1rem] pl-2 pt-2 md:pl-3 md:min-h-10 lg:text-xl"
                 placeholder="Mensaje..."
                 id=""
                 {...register("formBody", { required: true })}
                 aria-invalid={errors.email ? "true" : "false"}
               ></textarea>
-              {errors.email?.type === "required" && <p role="alert">Es necesario redactar un mensaje...</p>}
+              {errors.email?.type === "required" && (
+                <p className="bg-red-400 text-white pl-3 rounded-md w-1/3" role="alert">
+                  Es necesario redactar un mensaje...
+                </p>
+              )}
               <input type="submit" className="w-1/4 bg-black text-white rounded-md" />
             </form>
           </div>
